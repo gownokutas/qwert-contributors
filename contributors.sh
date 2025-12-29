f1a85ba30dc9476d1d24373a4ac0487a42d31cb3
linuxiarznaetacie <148643643+gownokutas@users.noreply.github.com>
2025-12-29 15:27:54 +0100

    Add files via upload

---
+++ contributors.sh
@@ -0,0 +1,38 @@
+# +------------------------------------------------------------+
+# CONTRBUTORS-SK
+# AUTORSTWA: Qwertyk12345
+# GITHUB: https://github.com/@gownokutas
+# DISCORD: hbfghfgh_24146
+# TELEGRAM: https://t.me/pomocinformatyczn
+# +------------------------------------------------------------+
+# NIE ZEZWALAM NA USUNIĘCIE TEGO WATERMARKA ANI MONETYZOWANIE
+# MOJEJ PRACY BEZ MOJEJ ZGODY!
+# +------------------------------------------------------------+
+
+while true; do
+    clear
+    echo "+------------------------------------------------------------+"
+    echo "           TUTAJ ZNAJDUJA SIE LINKI DO PROJEKTOW"
+    echo "       KTORE ZOSTALY UZYTE DO STWORZENIA MULTI-SKRYPTA"
+    echo "+------------------------------------------------------------+"
+    echo "NoVNC + Xfce4: "
+    echo "Pterodactyl Panel (github autora): https://github.com/jishnuthegamer"
+    echo "Instalator Docker'a: Autorski"
+    echo "Instalator Windows 10: https://github.com/dockur/windows (Skrypt jest autorski)"
+    echo "Menu CONTRIBUTORS-SK: Autorski"
+    echo "+------------------------------------------------------------+"
+    echo "NIE ZEZWALAM NA USUNIĘCIE TEGO WATERMARKA ANI MONETYZOWANIE"
+    echo "MOJEJ PRACY BEZ MOJEJ ZGODY!"
+    echo "  "
+    echo "© 2025 Linuxiarz na etacie :). Wszelkie prawa zastrzeżone.
+    echo "+------------------------------------------------------------+"
+    read -p "Nacisnij dowolny klawisz aby zamknac skrypt: " choice
+
+    case $choice in
+
+        *)
+            echo "Zamykanie skryptu..."
+            exit 0
+            ;;
+    esac
+done
