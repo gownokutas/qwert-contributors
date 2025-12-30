350c1822c4d94918e34dc3352c1430dbc9fa90e8
linuxiarznaetacie <148643643+gownokutas@users.noreply.github.com>
2025-12-30 23:08:12 +0100

    Refactor contributors.sh to remove infinite loop

--- contributors.sh
+++ contributors.sh
@@ -8,18 +8,16 @@
 # NIE ZEZWALAM NA USUNIĘCIE TEGO WATERMARKA ANI MONETYZOWANIE
 # MOJEJ PRACY BEZ MOJEJ ZGODY!
 # +------------------------------------------------------------+
-while true; do
-    clear
-    echo "+------------------------------------------------------------+"
-    echo "           TUTAJ ZNAJDUJA SIE LINKI DO PROJEKTOW"
-    echo "       KTORE ZOSTALY UZYTE DO STWORZENIA MULTI-SKRYPTA"
-    echo "+------------------------------------------------------------+"
-    echo "Instalator VM QEMU: https://github.com/gownokutas/firebase-vps-menu"
-    echo "NoVNC + Xfce4: https://github.com/vtstv/novnc-install"
-    echo "Pterodactyl Panel (github autora): https://github.com/jishnuthegamer"
-    echo "Instalator Docker'a: https://github.com/gownokutas/docker-installer"
-    echo "Instalator Windows 10: https://github.com/dockur/windows | https://github.com/gownokutas/install-windows"
-    echo "Menu CONTRIBUTORS-SK: https://github.com/gownokutas/qwert-contributors"
-    echo "+------------------------------------------------------------+"
-    sleep 5
 done
+clear
+echo "+------------------------------------------------------------+"
+echo "           TUTAJ ZNAJDUJA SIE LINKI DO PROJEKTOW"
+echo "       KTORE ZOSTALY UZYTE DO STWORZENIA MULTI-SKRYPTA"
+echo "+------------------------------------------------------------+"
+echo "Instalator VM QEMU: https://github.com/gownokutas/firebase-vps-menu"
+echo "NoVNC + Xfce4: https://github.com/vtstv/novnc-install"
+echo "Pterodactyl Panel (github autora): https://github.com/jishnuthegamer"
+echo "Instalator Docker'a: https://github.com/gownokutas/docker-installer"
+echo "Instalator Windows 10: https://github.com/dockur/windows | https://github.com/gownokutas/install-windows"
+echo "Menu CONTRIBUTORS-SK: https://github.com/gownokutas/qwert-contributors"
+echo "+------------------------------------------------------------+"
