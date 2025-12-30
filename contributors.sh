5a652affc4dac29afff67c9f12bf76f0e9be9a63
linuxiarznaetacie <148643643+gownokutas@users.noreply.github.com>
2025-12-30 23:02:16 +0100

    Update contributors.sh

--- contributors.sh
+++ contributors.sh
@@ -8,21 +8,18 @@
 # NIE ZEZWALAM NA USUNIĘCIE TEGO WATERMARKA ANI MONETYZOWANIE
 # MOJEJ PRACY BEZ MOJEJ ZGODY!
 # +------------------------------------------------------------+
-
 while true; do
     clear
     echo "+------------------------------------------------------------+"
     echo "           TUTAJ ZNAJDUJA SIE LINKI DO PROJEKTOW"
     echo "       KTORE ZOSTALY UZYTE DO STWORZENIA MULTI-SKRYPTA"
     echo "+------------------------------------------------------------+"
+    echo "Instalator VM QEMU: https://github.com/gownokutas/firebase-vps-menu"
     echo "NoVNC + Xfce4: https://github.com/vtstv/novnc-install"
     echo "Pterodactyl Panel (github autora): https://github.com/jishnuthegamer"
     echo "Instalator Docker'a: https://github.com/gownokutas/docker-installer"
     echo "Instalator Windows 10: https://github.com/dockur/windows | https://github.com/gownokutas/install-windows"
     echo "Menu CONTRIBUTORS-SK: https://github.com/gownokutas/qwert-contributors"
     echo "+------------------------------------------------------------+"
-    echo "NIE ZEZWALAM NA USUNIĘCIE TEGO WATERMARKA ANI MONETYZOWANIE"
-    echo "MOJEJ PRACY BEZ MOJEJ ZGODY!"
-    echo "  "
-    echo "© 2025 Linuxiarz na etacie :). Wszelkie prawa zastrzeżone.
-    echo "+------------------------------------------------------------+"
+    sleep 5
+done
